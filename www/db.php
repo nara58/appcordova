6cd38fb05b5ee63d03e1f9cd97c3ba6a0b615142
nara58 <80908498+nara58@users.noreply.github.com>
2021-03-24 13:28:07 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,5 +1,5 @@
 <?php
 header ("Access-Control-Allow-Origin: *");
-$con = mysqli_connect("localhost","id15158555_tugas3","Nisarahhma03.","tugas3") or die ("could not connect database");
+$con = mysqli_connect("localhost","id15158555_nis58","Nisarahhma03.","id15158555_tugas3") or die ("could not connect database");
 //please change the username, password and db name//
 ?>
